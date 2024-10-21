ce00fce720648954df0f72302ae43fbe2617b181
VitorCamilo01 <151784340+VitorCamilo01@users.noreply.github.com>
2024-10-21 16:32:28 -0300

    Add files via upload

---
+++ caQuestao2-P2/teste/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilha ventilador = new Pilha();
+
+            string[] pecas = new string[]
+            {
+            "Suporte",
+            "Hélice Quebrada",
+            "Lâmpada",
+            "Cúpula de Vidro"
+            };
+
+            foreach (var peca in pecas)
+            {
+                ventilador.Empilhar(peca);
+            }
+
+            Console.WriteLine("Ventilador com peça quebrada:");
+            ventilador.ImprimirPilha();
+
+            Console.WriteLine("\nQual peça deseja trocar?");
+            string velhaPeca = Console.ReadLine();
+
+            Console.WriteLine("Qual é a nova peça que irá substituir?");
+            string novaPeca = Console.ReadLine();
+
+            ventilador.SubstituirPeca(velhaPeca, novaPeca);
+
+            Console.WriteLine("\nVentilador após peça trocada:");
+            ventilador.ImprimirPilha();
+
+            Console.ReadLine();        }
+
+
+    }
+}
---
+++ caQuestao2-P2/teste/pilha.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    public class Pilha
+    {
+        private Stack<string> stack;
+
+        public Pilha()
+        {
+            stack = new Stack<string>();
+        }
+        public void Empilhar(string peca)
+        {
+            stack.Push(peca);
+        }
+        public string Desempilhar()
+        {
+            return stack.Pop();
+        }
+        public void SubstituirPeca(string velhaPeca, string novaPeca)
+        {
+            Stack<string> tempStack = new Stack<string>();
+            bool temp = false;
+            while (stack.Count > 0)
+            {
+                string pecaAtual = stack.Peek();
+                if (pecaAtual.Equals(velhaPeca, StringComparison.OrdinalIgnoreCase))
+                {
+                    stack.Pop(); 
+                    stack.Push(novaPeca); 
+                    temp = true;
+                    break;
+                }
+                else
+                {
+                    tempStack.Push(stack.Pop()); 
+                }
+            }
+
+            if (!temp)
+            {
+                Console.WriteLine("Peça não encontrada na pilha.");
+            }
+
+            while (tempStack.Count > 0)
+            {
+                stack.Push(tempStack.Pop());
+            }
+        }
+
+        public void ImprimirPilha()
+        {
+            Console.WriteLine("Estado atual da pilha:");
+            foreach (string peca in stack)
+            {
+                Console.WriteLine(peca);
+            }
+        }
+    }
+
+
+}
